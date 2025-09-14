f91c199fd5a2e9d61bbc9b80d593d88784897218
Darshan Bhamare <bhamaredarshan023@gmail.com>
2025-09-14 11:03:56 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Bot from './components/Bot'
+
+function App() {
+
+  // javascript code goes here!!
+
+  return (
+    <div><Bot/></div>
+  )
+}
+
+export default App
